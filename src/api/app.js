e032fa6ca3c8600d31929edcc259265cfe3d0960
Matheus Martino <matheus.martino97@gmail.com>
2021-07-15 16:53:52 -0300

    Requisito 1.

--- src/api/app.js
+++ src/api/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser').json();
+
+const { usersRoutes } = require('./routes');
+const { handleError } = require('../../middlewares');
 
 const app = express();
 
+app.use(bodyParser);
+
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+app.use('/users', usersRoutes);
+
+app.use(handleError);
+
 // Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
