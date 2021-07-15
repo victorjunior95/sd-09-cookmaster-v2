4bc2d560dfeb4945c6573a1699863f7af913d931
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-15 18:43:16 -0300

    feat: createUser service

---
+++ src/service/userService.js
@@ -0,0 +1,14 @@
+const UserModel = require('../model/userModel');
+const EmailAlreadyExistError = require('../errors/EmailAlreadyExistError');
+
+const create = async (name, email, password) => {
+  if (UserModel.isEmailRegistered(email)) throw new EmailAlreadyExistError();
+  const role = 'user';
+  const created = await UserModel.create(name, email, password, role);
+
+  return created;
+};
+
+module.exports = {
+  create,
+};
