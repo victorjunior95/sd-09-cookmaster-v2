9a023675a7f8a6a35f3ebc594c49fa712a87ef54
BernardoFontesNKWEB <bernardo.miranda@nkweb.com.br>
2021-07-15 16:55:19 -0300

    connection

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -1,13 +1,13 @@
 const mongoClient = require('mongodb').MongoClient;
 // A conexão do banco local deverá conter os seguintes parâmetros:
 
- const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+//const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
 // Para o avaliador funcionar altere a conexão do banco para:
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 const connection = () =>
   mongoClient
