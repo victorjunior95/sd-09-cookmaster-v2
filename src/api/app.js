2c2159c0a32094d0b3d5af83a6c9048c7747c4cd
matheusjkl1 <matheus.unilog@gmail.com>
2021-07-15 16:51:23 -0300

    first commit bora q bora

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
-
+const path = require('path');
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 // Não remover esse end-point, ele é necessário para o avaliador
 
 module.exports = app;
