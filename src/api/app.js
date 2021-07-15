b274db04d7847ecdc217f7f16f6e939cca4238e4
Khyrki <khyrkimitsunobi@gmail.com>
2021-07-15 15:46:17 -0300

    Inicia o Projeto

--- src/api/app.js
+++ src/api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-// Não remover esse end-point, ele é necessário para o avaliador
+// Não remover esse end-point, ele é necessário para o avaliador 
 app.get('/', (request, response) => {
   response.send();
 });
