0b06e0c57a588d5f7c5319192bb5b5dd142b04a0
brenno-calado <brenno.calado@gmail.com>
2021-07-15 15:15:04 -0300

    initial commit

--- src/api/app.js
+++ src/api/app.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const app = express();
 
-// Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
-// Não remover esse end-point, ele é necessário para o avaliador
 
 module.exports = app;
