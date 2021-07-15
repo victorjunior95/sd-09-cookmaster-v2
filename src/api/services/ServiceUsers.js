e7a387e8c462ca31738cf91b2b2e1b64c4619c03
Layo Kaminski <layokaminski@gmail.com>
2021-07-15 16:25:50 -0300

    Retirada de console.logs no ServiceUsers e ajustes de ESLint

--- src/api/services/ServiceUsers.js
+++ src/api/services/ServiceUsers.js
@@ -4,9 +4,8 @@ const invalidData = require('../utils/invalidData');
 const CONFLICT = 409;
 
 const create = async ({ name, email, password }) => {
-  console.log(email)
   const findEmail = await ModelUsers.getByEmail({ email });
-  console.log(findEmail)
+
   if (findEmail) throw invalidData('Email already registered', CONFLICT);
 
   const createdUser = await ModelUsers.create({ name, email, password });
