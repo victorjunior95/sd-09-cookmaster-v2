35931da750d78b5d917f45dd2410a913dd0af41f
Galaraz <jvictor.si.2013@gmail.com>
2021-07-15 18:09:18 -0300

    init

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const userRoutes = require('./Routes/user');
+const loginRoutes = require('./Routes/login');
+const recipeRoutes = require('./Routes/recipe');
 
 const app = express();
+const port = 3000;
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+app.use('/user', userRoutes);
+app.use('/login', loginRoutes);
+app.use('/recipe', recipeRoutes);
+
+app.listen(port, () => console.log(`CookMaster Server listening on port ${port}!`));
 
 module.exports = app;
