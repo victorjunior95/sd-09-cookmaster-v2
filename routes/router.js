398686b458146369bb00243cd56705bb71c0371a
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-15 15:13:32 -0300

    feat: add userController route

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const UserController = require('../controller/userController');
 
 const router = express.Router();
+
+router.use('/users', UserController);
 
 module.exports = router;
