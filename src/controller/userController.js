1362ac52389d3f14f1abff79781db3e08bca0879
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-15 18:59:54 -0300

    fix: statusCode pattern

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Validation = require('../middlewares/validation');
 const UserService = require('../service/userService');
 const ErrorHandler = require('../middlewares/errorHandler');
+const StatusCode = require('../statusCode');
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.post('/', Validation.createUser, async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
     const created = await UserService.create(name, email, password);
-    return res.status(201).json(created);
+    return res.status(StatusCode.created).json(created);
   } catch (err) {
     next(err);
   }
