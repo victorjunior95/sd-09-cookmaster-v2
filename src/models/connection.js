a560dc03d3fc3b9a703d2ef237b8784f44f3a33d
Diogenes <diogenesfelipe.s@outlook.com>
2021-07-15 14:49:09 -0300

    feat/creation of connection with mongodb

---
+++ src/models/connection.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb');
+
+const MONGODB_URI = 'mongodb://localhost:27017/Cookmaster';
+// const MONGODB_URI = 'mongodb://mongodb:27017/Cookmaster';
+
+const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+const DB_NAME = 'Cookmaster';
+
+const connection = async () => {
+  const connect = await MongoClient.connect(MONGODB_URI, OPTIONS);
+  const db = connect.db(DB_NAME);
+  return db;
+};
+
+module.exports = connection;
