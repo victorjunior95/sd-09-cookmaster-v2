a54da67c8c012d39fb4666e0a07c25f8f58dc702
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-15 18:41:11 -0300

    feat: create middleware errorHandler

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,14 @@
+const InvalidUserFormError = require('../errors/InvalidUserFormError');
+const EmailAlreadyExistError = require('../errors/EmailAlreadyExistError');
+const StatusCode = require('../statusCode');
+
+const errorHandler = (err, _req, res, _next) => {
+  if (err instanceof InvalidUserFormError) {
+    return res.status(StatusCode.badRequest).json({ message: err.message });
+  } 
+  if (err instanceof EmailAlreadyExistError) {
+    return res.status(StatusCode.badRequest).json({ message: err.message });
+  }
+};
+
+module.exports = errorHandler;
