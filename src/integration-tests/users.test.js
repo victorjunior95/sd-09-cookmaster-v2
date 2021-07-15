5df94642fc2e5e1177fc30f0d0aba95e68088e59
Enio Nicael <enionicael@gmail.com>
2021-07-15 16:24:41 -0300

    Escrita dos describes dos testes com base nos contratos.

---
+++ src/integration-tests/users.test.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const sinon = require('sinon');
+
+const chaiHTTP = require('chai-http');
+
+const { expect } = chai;
+
+describe('Cadastro de usuarios, GET /users', () => {
+  describe('Quando name, email e password não são informados', () => {
+
+  });
+  describe('Quando o cadastro nao e realizado com sucesso', () => {
+
+  });
+  describe('Quando o cadastro e realizado com sucesso', () => {
+
+  });
+});
