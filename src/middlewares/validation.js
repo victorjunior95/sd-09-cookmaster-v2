1a9635336a58f55c8bcfa7fcc1abb57691e1fb8c
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-15 16:35:47 -0300

    feat: create validation middleware, and createUser validation

---
+++ src/middlewares/validation.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+const InvalidUserFormError = require('../errors/InvalidUserFormError');
+
+const createUser = (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string()
+    .required(),
+    email: Joi.string()
+      .pattern('[^@]+@[^.]+.com$')
+      .required(),
+    password: Joi.string()
+    .required(),
+    role: Joi.string(),
+  });
+
+  const { name, email, password } = req.body;
+
+  const { error } = schema.validate({ name, email, password });
+
+  if (error) throw new InvalidUserFormError();
+
+  next();
+};
+
+module.exports = {
+  createUser,
+};
