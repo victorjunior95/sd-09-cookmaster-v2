575e546dcda3607d67642fa13326a6ff396ae551
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-15 15:06:34 -0300

    feat: use router

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const router = require('../../routes/router');
 
 const app = express();
 
@@ -7,5 +8,7 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(router);
 
 module.exports = app;
