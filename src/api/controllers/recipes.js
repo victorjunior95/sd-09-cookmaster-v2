be4ea3f88cc2579c3ba2d6db0b52ee6a63bf1c8e
brenno-calado <brenno.calado@gmail.com>
2021-07-15 22:59:57 -0300

    recipes controllers

---
+++ src/api/controllers/recipes.js
@@ -0,0 +1,20 @@
+const rescue = require('express-rescue');
+const service = require('../services/recipes');
+const { CREATED } = require('../constants/http.json');
+
+const create = rescue(async (request, response, next) => {
+  const { name, ingredients, preparation } = request.body;
+  const newRecipe = await service.create({ name, ingredients, preparation });
+  if (newRecipe.err) return next(newRecipe.err);
+  response.status(CREATED).json({
+    recipe: {
+      _id: newRecipe,
+      name,
+      ingredients,
+      preparation,
+      userId,
+    },
+  });
+});
+
+module.exports = { create };
