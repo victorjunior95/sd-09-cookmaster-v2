ea28d30774b4185608e74178bf813c05972ffa4e
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-16 17:18:28 -0300

    feat: add recipes route

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const UserController = require('../controller/userController');
 const LoginController = require('../controller/loginController');
+const RecipesController = require('../controller/recipeController');
 
 const router = express.Router();
 
 router.use('/users', UserController);
 
 router.use('/login', LoginController);
+
+router.use('/recipes', RecipesController);
 
 module.exports = router;
