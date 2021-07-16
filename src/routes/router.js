115e63687f139f25492eb7a363617ed140339285
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-16 14:42:35 -0300

    feat: add login to router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const UserController = require('../controller/userController');
+const LoginController = require('../controller/loginController');
 
 const router = express.Router();
 
 router.use('/users', UserController);
+
+router.use('/login', LoginController);
 
 module.exports = router;
