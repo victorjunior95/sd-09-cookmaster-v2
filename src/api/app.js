670bc591f9204c4d81d19947532f96e7a4c08f57
Carine Becker <caarine.m@gmail.com>
2021-07-16 15:14:49 -0300

    feat: config app

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-
-const app = express();
+const app = require('express')();
+const bodyParser = require('body-parser').json();
 
 // Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(bodyParser);
 
 module.exports = app;
