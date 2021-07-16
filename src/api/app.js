4902c2f6cac35d2103e2cad888c02415b6c4cfba
genadireuben <genadireuben91@gmail.com>
2021-07-16 19:48:26 -0300

    implementa endpoint de user e login

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const router = require('../routes/router');
 
 const app = express();
 
@@ -10,5 +11,13 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(router);
+
+app.use((err, _req, res, _next) => {
+  console.log(err.status);
+  if (err.message) return res.status(err.status).json({ message: err.message });
+  return res.status(500).json({ message: 'Internal server error' });
+});
 
 module.exports = app;
