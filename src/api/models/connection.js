77ed24c12418b3fd769111b75469768ad41066ce
Victor Breno <victor.brns2@gmail.com>
2021-07-16 16:03:38 -0300

    corrige connection para usar dotenv corretamente

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -6,8 +6,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const { MONGO_DB_URL } = process.env || 'mongodb://mongodb:27017/Cookmaster';
-const { DB_NAME } = process.env || 'Cookmaster';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = process.env.DB_NAME || 'Cookmaster';
 
 let db = null;
 
