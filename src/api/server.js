b7ddc0f4c56356101f99563a36d407a7e390a54d
Qu4k3r <neves0007@gmail.com>
2021-07-15 23:17:14 -0300

    Modifica variaveis de ambiente

--- src/api/server.js
+++ src/api/server.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const app = require('./app');
 
-const { PORT } = process.env || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
--- src/models/connection.js
+++ src/models/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { MongoClient } = require('mongodb');
 
-const { MONGO_DB_URL } = process.env || 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
