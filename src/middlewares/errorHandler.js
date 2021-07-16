e7f7df1f312ce176c8f751ff08c68b0764dfe1a1
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-16 19:22:05 -0300

    fix: get code from error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,7 +2,7 @@ const StatusCode = require('../statusCode');
 
 const errorHandler = (err, _req, res, _next) => {
   if (err.code) {
-    return res.status(err.StatusCode).json({ message: err.message });
+    return res.status(err.code).json({ message: err.message });
   }
 
   return res.status(StatusCode.internalServerError).json();
