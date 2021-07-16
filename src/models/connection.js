57088d7c75014fe7ba95ffb383f8acf79db491ca
Felipe Cardoso <felipedclc@gmail.com>
2021-07-16 20:25:33 -0300

    url bd

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
 const connection = () => MongoClient
