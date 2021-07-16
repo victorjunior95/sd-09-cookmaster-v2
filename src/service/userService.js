05e5dfacc748fd49f824610ce1a49c9d5fa38eda
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-16 15:01:51 -0300

    refactor: get error from index

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,8 +1,8 @@
 const UserModel = require('../model/userModel');
-const EmailAlreadyExistError = require('../errors/EmailAlreadyExistError');
+const Errors = require('../errors');
 
 const create = async (name, email, password) => {
-  if (await UserModel.isEmailRegistered(email)) throw new EmailAlreadyExistError();
+  if (await UserModel.isEmailRegistered(email)) throw new Errors.EmailAlreadyExistError();
   
   const role = 'user';
   const response = await UserModel.create(name, email, password, role);
