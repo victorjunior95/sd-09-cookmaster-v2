1812bb666eb04b305cb362ad275169ed815378a2
Vagner Pereira <sousa.vagner82@gmail.com>
2021-07-16 16:23:47 -0300

    requisito 1

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,12 +1,11 @@
 const { MongoClient } = require('mongodb');
 
 const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-const lOCALHOST_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
 const connection = () => MongoClient
-    .connect(lOCALHOST_DB_URL, {
+    .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
