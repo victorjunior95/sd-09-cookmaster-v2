7bcd45f74a3e388dadc12f6ef47a5bfd2fb15683
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-16 15:00:53 -0300

    feat: create error index

---
+++ src/errors/index.js
@@ -0,0 +1,9 @@
+const InvalidUserFormError = require('./InvalidUserFormError');
+const EmailAlreadyExistError = require('./EmailAlreadyExistError');
+const LoginFieldMissingError = require('./LoginFieldMissingError');
+
+module.exports = {
+  InvalidUserFormError,
+  EmailAlreadyExistError,
+  LoginFieldMissingError,
+};
