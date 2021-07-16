645554e90e7060cda3b162f3020a40d22c9ee893
Matheus Martino <matheus.martino97@gmail.com>
2021-07-16 11:40:57 -0300

    Arrumando o requisito 9.

--- src/api/routes/recipesRoutes.js
+++ src/api/routes/recipesRoutes.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (_req, _file, callback) => {
-    callback(null, path.join(__dirname, '..', 'uploads'));
+    callback(null, path.join(__dirname, '..', '..', 'uploads'));
   },
 
   filename: (req, _file, callback) => {
