fb7d6c2354be49624940f98fbd5a80ec712015c6
brenno-calado <brenno.calado@gmail.com>
2021-07-16 21:03:30 -0300

    tokenValidation

--- src/api/middlewares/tokenValidate.js
+++ src/api/middlewares/tokenValidate.js
@@ -1,26 +1,17 @@
 const jwt = require('jsonwebtoken');
+const Users = require('../models/users');
 
 const SECRET = 'mysohiddensecret';
 
-const tokenValidate = async (
-  request,
-  response,
-  next,
-) => {
-  const token = request
-    .headers.authorization;
-  if (!token) {
-    return next({
-      err: {
-        code: 'unauthorized',
-        message: 'jwt malformed',
-      },
-    });
-  }
+const tokenValidate = async (request, response, next) => {
+  const token = request.headers.authorization;
+  if (!token) return next({ err: { code: 'unauthorized', message: 'jwt malformed' } });
   try {
     const decode = await jwt.verify(token, SECRET);
+    const user = await Users.findByEmail(decode.data.email);
+    request.user = user;
   } catch (err) {
-
+    return { err: { code: 'unauthorized', message: err.message } };
   }
 };
 
