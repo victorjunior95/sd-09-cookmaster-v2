969ade8cc41e25d27281e1fa50cf0736fc7910f7
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-07-17 15:53:55 -0300

    Requisito 4 Ajustes lint

--- src/controllers/recipesControllers.js
+++ src/controllers/recipesControllers.js
@@ -1,18 +1,21 @@
 const recipesServices = require('../services/recipesServices');
 
-const { code: { CREATED, OK } } = require('../utils');
+const {
+  code: { CREATED, OK },
+  message: { RECIPE_NOT_FOUND },
+} = require('../utils');
 
 const createRecipes = async (req, res) => {
   const { name, ingredients, preparation } = req.body;
   const { userId } = req.user;
-
   const newRecipe = await recipesServices.createRecipes(name, ingredients, preparation, userId);
+
   return res.status(CREATED).json(newRecipe);
 };
 
 const getAllRecipes = async (req, res) => {
   const allRecipes = await recipesServices.getAllRecipes();
-  if (!allRecipes.length) return res.status(204).json({ message: RECIPE_NOT_FOUND});
+  if (!allRecipes.length) return res.status(204).json({ message: RECIPE_NOT_FOUND });
 
   return res.status(OK).json(allRecipes);
 };
