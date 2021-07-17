708f004ea27c9f80ec6b03c2fda950a0ef1e4d93
Matheus Martino <matheus.martino97@gmail.com>
2021-07-17 00:50:54 -0300

    Implementando tests.

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,11 +1,9 @@
 const usersRoutes = require('./usersRoutes');
 const loginRoutes = require('./loginRoutes');
 const recipesRoutes = require('./recipesRoutes');
-const imagesRoutes = require('./imagesRoutes');
 
 module.exports = {
   usersRoutes,
   loginRoutes,
   recipesRoutes,
-  imagesRoutes,
 };
