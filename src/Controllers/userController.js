aee19fe8adf8f4a631d16158a124bc727a6abf94
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-18 12:59:37 -0300

    requisito 1 quase completo - falta verificar se o email ja existe

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -1,11 +1,33 @@
-const userServices = require('../Services/user');
+// const { findByEmail } = require('../Models/userModel');
+const userServices = require('../Services/userService');
+
+const emptyNotAllowed = (name, email, password) => {
+    if (name === undefined || email === undefined || password === undefined) {
+        return true;
+    }
+    return false;
+};
+
+const testEmail = (email) => {
+    const rgx = /\S+@\S+\.\S+/;
+    // const user = await userServices.addUser(email);
+    // console.log(user.email, 'testEmail');
+    // if (user === null || user.email === undefined || user.email === null) {
+    //     return false;
+    // }
+    return rgx.test(email);
+};
 
 const addUser = async (req, res) => {
     const { name, email, password } = req.body;
+    const test = testEmail(email);
     const user = await userServices.addUser(name, email, password);
-    if (user !== null && user !== undefined) {
-        return res.status(200).json({ user });
-      }
+    if (emptyNotAllowed(name, email, password) === true || test === false) {
+        return res.status(400).json({ message: 'Invalid entries. Try again.' });
+    }
+    if (emptyNotAllowed(name, email, password) === false && test === true) {
+        return res.status(201).json({ user });
+    }
 };
 
 module.exports = {
