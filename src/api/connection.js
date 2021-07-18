03b8754a5618977fc9399e014fbd2be1d9dd66da
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-17 23:43:51 -0300

    connection - mongo

---
+++ src/api/connection.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+};
+
+const DB_NAME = 'StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+
+const connection = async () =>
+  MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => conn.db(DB_NAME)).catch(
+    (err) => {
+      console.error(err);
+      process.exit(1);
+    },
+  );
+
+module.exports = connection;
