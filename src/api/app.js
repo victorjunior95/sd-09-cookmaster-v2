2b2d1f97b9b2dc066343b017dcb9fa1e5334246b
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-17 23:41:22 -0300

    tinha faltado salvar esse

--- src/api/app.js
+++ src/api/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userController = require('../Controllers/userController')
+const userController = require('../Controllers/userController');
+
 const app = express();
+
 app.get('/', (request, response) => {
   response.send('aaa');
 });
 
 app.use(bodyParser.json());
-app.get('/users', userController.login )
 
-
+app.get('/users', userController.login);
 
 module.exports = app;
