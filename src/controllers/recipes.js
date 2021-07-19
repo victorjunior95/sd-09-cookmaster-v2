5969a5e03a439c2ae89f1a5febfba3ae4ac54a92
felipe <devfelipesantiago@gmail.com>
2021-07-19 11:05:12 -0300

    Create controllers to recipes

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -4,7 +4,30 @@ const createRecipe = rescue(async (req, res, _next) => {
   const addRecipe = req.body;
   return res.status(200).json(addRecipe);
 });
+const getRecipes = rescue(async (req, res, _next) => res.status(200).json(addRecipe));
+
+const getById = rescue(async (req, res, _next) => {
+  const { id } = req.params;
+  return res.status(200).json(id);
+});
+const updateRecipes = rescue(async (req, res, _next) => {
+  const { id } = req.params;
+  return res.status(200).json(id);
+});
+const updateImageRecipes = rescue(async (req, res, _next) => {
+  const image = req.params;
+  return res.status(200).json(image);
+});
+const deleteRecipes = rescue(async (req, res, _next) => {
+  const { id } = req.params;
+  return res.status(200).json(id);
+});
 
 module.exports = {
   createRecipe,
+  getRecipes,
+  getById,
+  updateRecipes,
+  updateImageRecipes,
+  deleteRecipes,
 };
