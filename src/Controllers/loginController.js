5b78bb183fafc46ae6dcbe03c38d93c1ffa92fac
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-18 22:44:44 -0300

    requisito 3

--- src/Controllers/loginController.js
+++ src/Controllers/loginController.js
@@ -7,7 +7,7 @@ const responses = {
     invalid: 'Incorrect username or password',
 };
 
-const JWT_SECRET = 'meuSegredoSuperSegreto';
+const JWT_SECRET = 'meuSegredoSuperSecreto';
 
 const validateBody = async (body) => {
     if (body.password === undefined || body.email === undefined) {
@@ -26,13 +26,9 @@ const login = async (req, res, _next) => {
     } if (validate === responses.invalid) {
         return res.status(401).json({ message: responses.invalid });
     }
-    const payload = {
-        email: req.body.email,
-        password: req.body.password,
-    };
-
-    const token = jwt.sign(payload, JWT_SECRET, {
-        expiresIn: '1h',
+    const user = await userModel.findByEmail(req.body.email);
+    const token = jwt.sign({ user }, JWT_SECRET, {
+        expiresIn: '1h', algorithm: 'HS256',
     });
 
     res.status(200).json({ token });
--- src/api/app.js
+++ src/api/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const userController = require('../Controllers/userController');
 const loginController = require('../Controllers/loginController');
 const recipesController = require('../Controllers/recipesController');
+const { auth } = require('../Middlewares/recipesMidlleware');
 
 const app = express();
 
@@ -14,6 +15,6 @@ app.use(bodyParser.json());
 
 app.post('/users', userController.addUser);
 app.post('/login', loginController);
-app.post('/recipes', recipesController.addRecipe);
+app.post('/recipes', auth, recipesController.addRecipe);
 
 module.exports = app;
