9d90fb2e9c18b44db1584cf4d84bfa26a8fe1b02
Lucas Silva <martssilvapro@gmail.com>
2021-07-19 09:32:57 -0300

    implementa seed

--- seed.js
+++ seed.js
@@ -1,1 +1,11 @@
-// colocar query do MongoDB
+const connection = require('./src/api/models/connection');
+
+const registerAdmin = (name, email, password) => connection()
+  .then((db) => db.collection('users').insertOne(
+    { name, email, password, role: 'admin' },
+  ))
+  .then((result) => result.ops[0]);
+
+module.exports = {
+  registerAdmin,
+}
