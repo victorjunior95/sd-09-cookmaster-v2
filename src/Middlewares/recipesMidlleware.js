bd757d7a0dbb1c0db521a2f4774f7bfe2d75ddaa
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-18 22:46:42 -0300

    requisito 3

---
+++ src/Middlewares/recipesMidlleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const missingAuthToken = { message: 'missing auth token' };
+const JWTmalformed = { message: 'jwt malformed' };
+
+const auth = async (req, res, next) => {
+  const token = req.headers.authorization;
+  const JWT_SECRET = 'meuSegredoSuperSecreto';
+  console.log(token, 'token');
+  if (!token) {
+    res.status(401).json(missingAuthToken);
+  }
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    console.log(payload, 'payload');
+    if (!payload) {
+      return res.status(401).json(JWTmalformed);
+    } 
+      req.user = payload;
+      next();
+  } catch (error) {
+    return res.status(401).json(JWTmalformed);
+  }
+};
+
+module.exports = {
+  auth,
+};
