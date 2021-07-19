47b76c4926799f59d9753f0bfc72cfc2cb730393
brenno-calado <brenno.calado@gmail.com>
2021-07-19 16:06:56 -0300

    refactor error middleware

--- src/api/middlewares/error.js
+++ src/api/middlewares/error.js
@@ -1,13 +1,7 @@
-const { BAD_REQUEST, CONFLICT, UNAUTHORIZED, NOT_FOUND } = require('../constants/http.json');
+const http = require('../constants/http.json');
 
 module.exports = (err, _request, response, _next) => {
-  if (err) {
-    switch (err.code) {
-      case 'invalid_data': return response.status(BAD_REQUEST).json(err);
-      case 'existing_email': return response.status(CONFLICT).json(err);
-      case 'unauthorized': return response.status(UNAUTHORIZED).json(err);
-      case 'not_found': return response.status(NOT_FOUND).json(err);
-      default: response.status(500).json(err);
-    }
-  }
+  const { code } = err;
+  if (!code) return response.status(500).json(err);
+  return response.status(http[code]).json(err);
 };
