301806d54504e6e4877511b923506254367a6862
Enio Nicael <enionicael@gmail.com>
2021-07-19 01:47:15 -0300

    Requisito 2, implementacao da geracao de token com jwt na rota de login.

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,5 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'senhadanasa';
+
 const loginModel = require('../models/loginModel');
 const { validateError, schema } = require('./schemas/loginSchema');
+
+const jwtConfig = {
+  expiresIn: '15m',
+  algorithm: 'HS256',
+};
 
 const login = async (email, password) => {
   const { error } = schema.validate({ email, password });
@@ -10,8 +19,12 @@ const login = async (email, password) => {
 
   if (!result) throw validateError(401, 'Incorrect username or password');
 
+  const { password: _, ...userData } = result;
+
+  const token = jwt.sign(userData, secret, jwtConfig);
+
   // retornar token
-  return { token: result };
+  return { token };
 };
 
 module.exports = { login };
