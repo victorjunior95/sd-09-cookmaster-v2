27fe17bf78d90b59917c6835d462bf51abf28c2c
felipe <devfelipesantiago@gmail.com>
2021-07-19 10:59:16 -0300

    Create router for recipes

--- src/router/recipes/index.js
+++ src/router/recipes/index.js
@@ -5,5 +5,10 @@ const recipeRouter = express.Router();
 const createRecipe = require('../../controllers/recipes');
 
 recipeRouter.post('/', createRecipe);
+recipeRouter.get('/', getRecipes);
+recipeRouter.get('/:id', getById);
+recipeRouter.put('/:id', updateRecipes);
+recipeRouter.put('/:id/image', updateImageRecipes);
+recipeRouter.delete('/:id', deleteRecipes);
 
 module.exports = recipeRouter;
