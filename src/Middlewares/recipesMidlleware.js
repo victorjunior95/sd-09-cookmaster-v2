5bc396844e18b4c9d4932e3354030b0774d0bbac
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-20 15:55:20 -0300

    correçao de autenticaçaoo. agora sim ta ceerto

--- src/Middlewares/recipesMidlleware.js
+++ src/Middlewares/recipesMidlleware.js
@@ -7,18 +7,15 @@ const auth = async (req, res, next) => {
   const token = req.headers.authorization;
   const JWT_SECRET = 'meuSegredoSuperSecreto';
   if (!token) {
-    res.status(401).json(missingAuthToken);
+   return res.status(401).json(missingAuthToken);
   }
   try {
-    if (token !== undefined) {
     const payload = jwt.verify(token, JWT_SECRET);
-    console.log(payload, 'payload');
     if (!payload) {
       return res.status(401).json(JWTmalformed);
     } 
       req.user = payload;
       next();
-  }
   } catch (error) {
     return res.status(401).json(JWTmalformed);
   }
