bae6352e1dc57b9dce05543b4286c7300ef60740
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-20 16:57:27 -0300

    começando post de imagem

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const userController = require('../Controllers/userController');
 const loginController = require('../Controllers/loginController');
 const recipesController = require('../Controllers/recipesController');
@@ -12,6 +13,7 @@ app.get('/', (request, response) => {
 });
 
 app.use(bodyParser.json());
+const upload = multer({ dest: 'uploads/' });
 
 app.post('/users', userController.addUser);
 app.post('/login', loginController);
@@ -20,4 +22,8 @@ app.get('/recipes/:id', recipesController.getRecipe);
 app.get('/recipes', recipesController.getAllRecipes);
 app.put('/recipes/:id', auth, recipesController.updateRecipe);
 app.delete('/recipes/:id', auth, recipesController.delRecipe);
+app.post('/recipes/:id/image',  upload.single('file'), (req, res) => {
+  res.send('OK');
+});
+
 module.exports = app;
