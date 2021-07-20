35ee887f36075d363879fede5cdd2f8ba0135037
MarceloBorges <melombm@hotmail.com>
2021-07-19 21:10:50 -0500

    requisito 2 lint

--- src/middlewares/validateJWT.js
+++ src/middlewares/validateJWT.js
@@ -11,24 +11,24 @@ const recipesJWT = async (req, res, next) => {
   if (!token) {
     return res.status(status.UNAUTHORIZED).json({ message: status.MISSING });
   }
-  
+
   try {
     const decode = jwt.verify(token, secret);
 
     if (!decode) {
       return res.status(status.UNAUTHORIZED).json({ message: status.MALFORMED });
     }
-    
-      // adcionar uma nova informação
 
-      const modelsUser = await userModels.getByEmail(decode.data);
-      if (modelsUser) {
-        const { password, ...other } = modelsUser;
-        // console.log(other);
-        req.user = other;
-      }
+    // adcionar uma nova informação
 
-      return next();
+    const modelsUser = await userModels.getByEmail(decode.data);
+    if (modelsUser) {
+      const { password, ...other } = modelsUser;
+      // console.log(other);
+      req.user = other;
+    }
+
+    return next();
   } catch (err) {
     return res.status(status.UNAUTHORIZED).json({ message: status.MALFORMED, catch: 'linha 33' });
   }
@@ -36,4 +36,5 @@ const recipesJWT = async (req, res, next) => {
 
 module.exports = {
   recipesJWT,
+
 };
--- src/services/loginService.js
+++ src/services/loginService.js
@@ -7,40 +7,18 @@ const jwtConfig = {
   algorithm: 'HS256',
 };
 
-const verifyPassAndMail = (mail, pass) => {
-  if (!pass || !mail) { return undefined; }
-};
-
 const login = async (mail, pass) => {
   const byEmail = await userModels.getByEmail(mail);
-  verifyPassAndMail(mail, pass);
+  if (!pass || !mail) { return undefined; }
   if (byEmail !== null) {
     const { password, email } = byEmail;
     if (password === pass) {
       const token = jwt.sign({ data: email }, secret, jwtConfig);
       return token;
     }
-  } else {
-    return 'null';
   }
+  return 'null';
 };
-
-// const login = async (mail, pass) => {
-//   const byEmail = await userModels.getByEmail(mail);
-
-//   if (!pass || !mail) { return undefined; }
-
-//   if (byEmail !== null) {
-//     const { password, email } = byEmail;
-
-//     if (password === pass) {
-//       const token = jwt.sign({ data: email }, secret, jwtConfig);
-//       return token;
-//     }
-//   } else {
-//     return 'null';
-//   }
-// };
 
 module.exports = {
   login,
