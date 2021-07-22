943c8e156ac23eb036b4744c4f8d6ff99e2d63d9
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-07-22 16:23:57 -0300

    novo push pra ver se o avaliador resolve trabalhar direito

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,6 +3,8 @@ const usersService = require('../services/usersService');
 const DEFAULT_SUCCESS_STATUS = 200;
 const CREATE_SUCCESS_STATUS = 201;
 
+// colocando qualquer coisa só pra dar um novo push
+
 const createUser = async (req, res, next) => {
   try {
     const user = req.body;
