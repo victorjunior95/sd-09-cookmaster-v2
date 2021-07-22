5cb545636a1d7980cb947536c6c602f477b355b5
victorjunior95 <victorjr.muchacho@gmail.com>
2021-07-22 17:25:39 -0300

    Endpoint for user registration: second correction - challenge 1

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const usersService = require('../services/UsersService');
+// const usersService = require('../services/UsersService');
 
 const UserRouter = express.Router();
 
-UserRouter.post('/', (req, res) => {
-  const { name, email, password } = req.body;
-  // console.log(name, email, password);
-  const user = { name, email, password, role: 'user' };
-  const { status, newUser } = usersService.register(user);
+UserRouter.post('/', (req, _res) => {
+  console.log(req.body);
+  // const { name, email, password } = req.body;
+  // const user = { name, email, password, role: 'user' };
+  // const { status, newUser } = usersService.register(user);
 
-  res.status(status).json(newUser);
+  // res.status(status).json(newUser);
 });
 
 module.exports = UserRouter;
