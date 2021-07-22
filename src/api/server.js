655237c81e0e74142f1baa3dfb08b623d04e5a0c
felipe <devfelipesantiago@gmail.com>
2021-07-22 13:42:34 -0300

    test

--- src/api/server.js
+++ src/api/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-const { PORT } = process.env || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
