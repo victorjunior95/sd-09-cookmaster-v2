e499a84eeef7a6a1b8674e5c6608513953388a2d
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-07-23 16:49:42 -0300

    req. 03

--- src/api/models/recipeModel.js
+++ src/api/models/recipeModel.js
@@ -1,9 +1,9 @@
 const connection = require('./connection');
 
 const createRecipe = async ({ name, preparation, ingredients }) => {
-  const usersCollection = await connection()
+  const recipe = await connection()
     .then((db) => db.collection('recipes'));
-  const { insertedId } = await usersCollection
+  const { insertedId } = await recipe
   .insertOne({ name, preparation, ingredients, url: 'url' });
 
   return {
