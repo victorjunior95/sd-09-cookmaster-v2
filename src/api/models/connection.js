79e77ec7638640b7ede79118f8078a2352421b25
williamDieter88 <williamzdieter@gmail.com>
2021-07-22 22:13:49 -0300

    Test 80%

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 
 const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
-
+//
 const connection = () => MongoClient
     .connect(MONGO_DB_URL, {
       useNewUrlParser: true,
