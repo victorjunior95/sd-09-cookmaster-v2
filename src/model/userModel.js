b76b63860de47d0e0f18fe1824882ad1c42e1047
antonioarieiro <sfgfffs@gmail.com>
2021-07-24 13:20:22 -0300

    fix errs

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,4 +1,4 @@
-const connection = require('./connection.js');
+const connection = require('./connection');
 
 const createNewUser = async (email, name, password) => {
   const user = await connection().then((db) =>
