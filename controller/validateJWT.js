455f6f6022f39f095b33dab52b1d3d9a6088c7de
Carine Becker <caarine.m@gmail.com>
2021-07-26 15:58:17 -0300

    fixes

--- controller/validateJWT.js
+++ controller/validateJWT.js
@@ -7,13 +7,18 @@ module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) throw res.status(401).json({ message: 'missing auth token' });
   try {
-    jwt.verify(token, secret, (err, decoded) => {
-      if (err) throw res.status(401).json({ message: 'jwt malformed' });
-      const user = usersModel.findEmail(decoded.data.email);
-      req.user = user;
-    });
+    const decoded = jwt.verify(token, secret);
+    if (!decoded) throw res.status(401).json({ message: 'jwt malformed' });
+    const user = usersModel.findEmail(decoded.data.email);
+    req.user = user;
     next();
   } catch (err) {
     return next(err);
   }
 };
+
+// jwt.verify(token, secret, (err, decoded) => {
+//   if (err) return res.status(401).json({ message: 'jwt malformed' });
+//   const user = usersModel.findEmail(decoded.data.email);
+//   req.user = user;
+// });
--- routes/recipes.js
+++ routes/recipes.js
@@ -3,11 +3,11 @@ const recipesController = require('../controller/recipesController');
 
 const router = express.Router();
 
+router.post('/', recipesController.postRecipe);
+router.get('/', recipesController.getRecipes);
 router.get('/:id', recipesController.getRecipeById);
 router.put('/:id', recipesController.putRecipe);
 router.delete('/:id', recipesController.deleteRecipe);
 router.put('/:id/image/', recipesController.uploadRecipeImage);
-router.get('/', recipesController.getRecipes);
-router.post('/', recipesController.postRecipe);
 
 module.exports = router;
