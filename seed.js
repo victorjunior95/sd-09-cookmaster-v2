eb9f7806b40fbc410b981f7066a739080090344c
Enio Nicael <enionicael@gmail.com>
2021-07-26 15:18:07 -0300

    Requisito 6, refatoracao de codigo no arquivo seed.

--- seed.js
+++ seed.js
@@ -1,20 +1,3 @@
 // colocar query do MongoDB
 
-const userModel = require('./src/models/usersModel');
-
-const adminData = {
-  name: 'admin',
-  email: 'root@email.com',
-  password: 'admin',
-  role: 'admin'
-};
-
-const createUserAdmin = async (adminData) => {
-  try {
-    await userModel.createUserAdmin(adminData);
-  } catch (err) {
-    console.log(`[Seed error] > ${err.message}`);
-  }
-}
-
-createUserAdmin(adminData);
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
