dd266a23e55a7e25b4f004442c7870c111c017b9
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-07-27 18:37:38 -0300

    Requisito 6

--- seed.js
+++ seed.js
@@ -1,1 +1,5 @@
 // colocar query do MongoDB
+const cookmasterDb = require('./src/model/cookmasterDb');
+
+const db = await cookmasterDb();
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
