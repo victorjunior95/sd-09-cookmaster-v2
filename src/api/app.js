b5d65a345f10e2f698ff9551703d9baa0efbd4c2
RodrigoNeoEx <draja.acc@gmail.com>
2021-07-28 11:48:11 -0300

    created routes on app, project ready to start development

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const user = require('../routes/userRoutes');
+const login = require('../routes/loginRoutes');
+const recipes = require('../routes/recipesRoutes');
 
 const app = express();
+app.use(bodyParser.json());
+
+const PORT = 3000;
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/users', user);
+app.use('/login', login);
+app.use('/recipes', recipes);
+app.use('/images', express.static('../uploads/'));
+
+app.use(({ status, message }, _req, res, _next) => {
+  res.status(status).json({ message });
+});
+
+app.listen(PORT, () => {
+  console.log('Server localhost:3000 is Online');
+});
 
 module.exports = app;
