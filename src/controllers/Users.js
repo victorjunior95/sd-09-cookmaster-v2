4b2e294c32f70cd6193604a0cd484dd92d9bb578
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-07-28 22:22:29 -0300

    lint issues

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -7,7 +7,7 @@ const createUser = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
+  // const { email, password } = req.body;
   res.status(200).json({ token: '123345' });
 };
 
