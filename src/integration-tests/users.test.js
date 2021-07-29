98cbfa563c67cb1de8cfd68bd3a45a8e556da7d3
Carine Becker <caarine.m@gmail.com>
2021-07-29 13:59:07 -0300

    integration tests

--- src/integration-tests/users.test.js
+++ src/integration-tests/users.test.js
@@ -1,5 +1,35 @@
 const chai = require('chai');
-const sinon = require('sinon');
+const chaiHttp = require('chai-http');
 
-describe('', () => {
+const app = require('../api/app');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('POST /users', () => {
+  describe('When user is registered', () => {
+    let response;
+
+    before (async () => {
+      response = await chai.request(app).post('/users').send({
+        name: 'Teste Foi',
+        email: 'test_foi@test.com',
+        password: '12345678',
+      });
+    })
+
+    it('return HTPP 201', () => {
+      expect(response).to.have.status(201);
+    });
+
+    it('return user object', () => {
+      expect(response.body).to.be.an('object');
+    });
+
+    it('return user info', () => {
+      expect(response.body).to.have.property('user');
+    });
+
+  });
 });
