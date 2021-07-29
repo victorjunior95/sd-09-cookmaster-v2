a33bdd55d5e88e50f6f785c1ff4961ddbdc75ef2
Carine Becker <caarine.m@gmail.com>
2021-07-29 12:28:25 -0300

    integration tests

--- src/integration-tests/login.test.js
+++ src/integration-tests/login.test.js
@@ -1,12 +1,12 @@
 const chai = require('chai');
 const sinon = require('sinon');
-
 const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
 
 const app = require('../api/app');
-// const { getConnection } = require('./connectionMock');
-// const { MongoClient } = require('mongodb');
+const { getConnection } = require('./connectionMock');
+const { MongoClient } = require('mongodb');
+
+chai.use(chaiHttp);
 
 const { expect } = chai;
 
@@ -39,30 +39,73 @@ describe('POST /login', () => {
   });
 
   describe('When email doesn`t exists', () => {
-    it('return HTTP 401', () => {
+    let connectionMock;
+    let response;
 
+    before(async () => {
+      connectionMock = await getConnection();
+      sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+
+      response = await chai.request(app).post('/login').send({
+        email: 'test@test.com',
+        password: '12345678',
+      });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+    });
+
+    it('return HTPP 401', () => {
+      expect(response).to.have.status(401);
     });
 
     it('return body', () => {
-
+      expect(response.body).to.be.an('object');
     });
 
     it('response object has `message` property', () => {
-
+      expect(response.body).to.have.property('message');
     });
 
     it('message property has its proper message ', () => {
-
+      expect(response.body.message).to.be.equal('Incorrect username or password')
     });
   });
 
-  describe('When login is successful', () => {
-    it('return HTTP 200', () => {
+  describe.skip('When login is successful', () => {
+    let connectionMock;
+    let response;
 
+    before(async () => {
+      connectionMock = await getConnection();
+      sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+
+      await connectionMock.db('Cookmaster').collection('users').insertOne({
+        name: 'Teste Foi',
+        email: 'test_foi@test.com',
+        password: '12345678'
+      });
+
+      response = await chai.request(app).post('/login').send({
+        email: 'test_foi@test.com',
+        password: '12345678',
+      });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await connectionMock.db('Cookmaster').collection('users').deleteOne({
+        email: 'test_foi@test.com',
+      })
+    });
+
+    it('return HTPP 200', () => {
+      expect(response).to.have.status(200);
     });
 
     it('return token', () => {
-
+      expect(response.body).to.have.property('token')
     });
   });
 });
