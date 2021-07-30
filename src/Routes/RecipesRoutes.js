dc97564882be2003b7a28ba5756f75aa585a0b81
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-07-30 00:32:57 -0300

    requisito 8 fix mongo url 2

--- src/Routes/RecipesRoutes.js
+++ src/Routes/RecipesRoutes.js
@@ -11,6 +11,7 @@ const RecipesRoutes = (app) => {
   app.route('/recipes')
     .get(fetchRecipes)
     .post(validateToken, rescue(createRecipe));
+
   app.route('/recipes/:id')
     .get(rescue(findById))
     .put(validateToken, editRecipe)
