0ecf329d2fce654c0731a993740356c980605e54
Ian Zwanck Goodwin <ianzwanck@yahoo.com.br>
2021-08-05 16:42:47 -0300

    req 9 uploads

--- controller/Storage.js
+++ controller/Storage.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const Recipe = require('../service/Recipe');
 
 module.exports = multer.diskStorage({ 
-  destination: (_req, _file, callback) => callback(null, 'uploads'),
+  destination: (_req, _file, callback) => callback(null, 'src/uploads'),
   filename: async (req, _file, callback) => {
     const { id } = req.params;
     const recipe = await Recipe.findById(id);
