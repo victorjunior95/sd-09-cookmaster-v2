97c22a31a83985046c1104c74f28d236e4cfe80b
rodolfo_oliveira <rod.vgo@gmail.com>
2021-08-07 13:14:25 -0300

    teste integraçao

--- src/api/routes/recipes.js
+++ src/api/routes/recipes.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const recipes = require('../controllers/recipes');
+const Controllers = require('../controllers/RecipesController');
 const validate = require('../middlewares/validators');
 
 const route = express.Router();
 
-route.post('/', validate.token, recipes.create);
-route.get('/', recipes.getAll);
-route.get('/:id', recipes.getById);
-route.put('/:id', validate.token, recipes.update);
-route.delete('/:id', validate.token, recipes.remove);
-route.put('/:id/image', validate.token, recipes.putImage);
+route.post('/', validate.token, Controllers.create);
+route.get('/', Controllers.getAll);
+route.get('/:id', Controllers.getById);
+route.put('/:id', validate.token, Controllers.update);
+route.delete('/:id', validate.token, Controllers.remove);
+route.put('/:id/image', validate.token, Controllers.putImage);
 
 module.exports = route;
