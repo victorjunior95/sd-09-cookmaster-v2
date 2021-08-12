33246a503c3bf0ffbdf1bbae0a4f326c38ddf1f8
fomes <contato.felipeluna@gmail.com>
2021-08-12 16:14:56 -0300

    fix dot notation lint

--- src/api/auth/validateJWT.js
+++ src/api/auth/validateJWT.js
@@ -4,7 +4,7 @@ const userModels = require('../../models/userModels');
 const secret = 'seusecretdetoken';
 
 module.exports = async (req, res, next) => {
-  const token = req.headers['authorization'];
+  const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ message: 'missing auth token' });
