00a3098d1bdef6b8246a8de5596a6e4cddf36dbd
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-08-12 21:08:30 -0300

    feito req10

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.get('/', (request, response) => {
 app.use('/users', require('./users/users.routes'));
 app.use('/login', require('./login/login.routes'));
 app.use('/recipes', require('./recipes/recipes.routes'));
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 module.exports = app;
