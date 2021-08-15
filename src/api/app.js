8f4e489dd6a248f5908b9c8caac228d216b0bdea
Uibirá <usbarreto@gmail.com>
2021-08-15 16:22:50 -0300

    Mudanças aleatorias sem sentido nenhum

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-// const bodyParser = require('body-parser');
-
 const app = express();
 
 app.use(bodyParser.json());
