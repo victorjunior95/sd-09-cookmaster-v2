239f3c20f6169fdfdd97537794a80029257a9496
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-08-16 00:08:44 -0300

    Requisito 10

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 const routes = require('../routes');
 
 const app = express();
+
+app.use('/images', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.use(bodyParser.json());
 
