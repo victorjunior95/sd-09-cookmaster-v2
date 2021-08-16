2126bfd21ddbdafaf9d63c639b288aa7ce4a09b4
victorjunior95 <victorjr.muchacho@gmail.com>
2021-08-16 09:30:23 -0300

    resolvido

--- src/api/auth/validateAdmin.js
+++ src/api/auth/validateAdmin.js
@@ -1,24 +1,12 @@
-const jwt = require('jsonwebtoken');
-const KEY = require('../../utils/secret');
+// const jwt = require('jsonwebtoken');
+// const KEY = require('../../utils/secret');
 
 const FORBIDDEN = 403;
 
 module.exports = async (req, res, next) => {
-  // const admin = async ({ authorization }) => {
-  //   const secret = '60f25632bbd8eb246fbe3170';
-  //   const { role } = jwt.verify(authorization, secret);
-  //   if (role !== 'admin') {
-  //     const err = { message: 'Only admins can register new admins' };
-  //     throw err;
-  //   }
-  // };
+  const { role } = req.user;
+  console.log(req.user);
 
-  try {
-    const { authorization } = req.headers;
-    const { role } = jwt.verify(authorization, KEY);
-
-    if (role === 'admin') next();
-  } catch (error) {
-    return res.status(FORBIDDEN).json({ message: 'Only admins can register new admins' });
-  }
+  if (role === 'admin') return next();
+  return res.status(FORBIDDEN).json({ message: 'Only admins can register new admins' });
 };
