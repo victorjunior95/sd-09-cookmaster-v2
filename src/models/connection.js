d23a07fd087f187f90949c8fc08ad309c4722307
Isabella Paz <bellapaz.iorp@gmail.com>
2021-08-16 12:54:49 -0300

    connection

---
+++ src/models/connection.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+
+const DB_NAME = 'Cookmaster';
+const MONGO_DB_URL = `mongodb://mongodb:27017/${DB_NAME}`;
+// const MONGO_DB_URL = `mongodb://localhost:27017/${DB_NAME}`;
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connection = () => MongoClient
+  .connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => conn.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+module.exports = connection;
