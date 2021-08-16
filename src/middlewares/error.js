37280d34a60331770bf4ab7de5ad887a9ef4cdc5
Uibirá <usbarreto@gmail.com>
2021-08-15 23:02:06 -0300

    requisito. eslint

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,11 +1,11 @@
 const BADREQUEST = 400;
 const UNAUTHORIZED = 401;
-const FORBIDDEN = 403;
-const NOTFOUND = 404;
+// const FORBIDDEN = 403;
+// const NOTFOUND = 404;
 const CONFLICT = 409;
-const INTERNALSERVERERROR = 500;
+// const INTERNALSERVERERROR = 500;
 
-module.exports = (err, _req, res, _next) => {
+const error = (err, _req, res, _next) => {
   if (err.message === 'Invalid entries. Try again.') {
     return res.status(BADREQUEST).json({ message: err.message });
   }
@@ -18,17 +18,19 @@ module.exports = (err, _req, res, _next) => {
   if (err.message === 'Incorrect username or password') {
     return res.status(UNAUTHORIZED).json({ message: err.message });
   }
-  if (err.name === 'JsonWebTokenError') {
-    return res.status(UNAUTHORIZED).json({ message: 'jwt malformed' });
-  }
-  if (err.message === 'recipe not found') {
-    return res.status(NOTFOUND).json({ message: err.message });
-  }
-  if (err.message === 'missing auth token') {
-    return res.status(UNAUTHORIZED).json({ message: err.message });
-  }
-  if (err.message === 'Only admins can register new admins') {
-    return res.status(FORBIDDEN).json({ message: err.message });
-  }
-  res.status(INTERNALSERVERERROR).json({ message: 'Erro interno', error: err.message });
+  // if (err.name === 'JsonWebTokenError') {
+  //   return res.status(UNAUTHORIZED).json({ message: 'jwt malformed' });
+  // }
+  // if (err.message === 'recipe not found') {
+  //   return res.status(NOTFOUND).json({ message: err.message });
+  // }
+  // if (err.message === 'missing auth token') {
+  //   return res.status(UNAUTHORIZED).json({ message: err.message });
+  // }
+  // if (err.message === 'Only admins can register new admins') {
+  //   return res.status(FORBIDDEN).json({ message: err.message });
+  // }
+  // res.status(INTERNALSERVERERROR).json({ message: 'Erro interno', error: err.message });
 };
+
+module.exports = { error };
