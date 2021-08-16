d2fb97a6f04a83c204d6382cefcc74abfecc2239
Isabella Paz <bellapaz.iorp@gmail.com>
2021-08-16 12:49:48 -0300

    app

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-// Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
-  response.send();
-});
-// Não remover esse end-point, ele é necessário para o avaliador
+const routes = require('./routes');
+
+app.use(bodyParser.json());
+app.use('', routes);
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+app.get('/', (request, response) => { response.send(); });
 
 module.exports = app;
